fe40b9b55dde59de76f16d1b17d07010cfd68dcd
icedwolf <romaxino@gmail.com>
2024-06-06 17:12:59 -0300

    trying to fix the needrestart apt

--- telegraf.sh
+++ telegraf.sh
@@ -38,11 +38,10 @@ esac
 
 case $pkg_mng in
 apt)
-	DEBIAN_FRONTEND=noninteractive
 	curl -s https://repos.influxdata.com/influxdata-archive_compat.key >influxdata-archive_compat.key
 	echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg >/dev/null
 	echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | tee /etc/apt/sources.list.d/influxdata.list
-	apt-get update && apt-get install --no-install-recommends --no-upgrade -y telegraf
+	DEBIAN_FRONTEND=noninteractive NEEDRESTART_MODE=a apt-get update && apt-get install --no-install-recommends --no-upgrade -y telegraf
 	;;
 yum)
 	cat <<EOF | tee /etc/yum.repos.d/influxdb.repo
